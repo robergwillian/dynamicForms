51f3b25492155eaa8d4e075a2b16fbf846782d65
Willian Roberg de Melo <roberg.willian@gmail.com>
2022-08-21 10:33:45 -0300

    code refactor

--- styles/index.styles.js
+++ styles/index.styles.js
@@ -4,8 +4,6 @@ export const styles = {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    width: "100vw",
-    height: "100vh",
   },
   formContainer: {
     display: "flex",
